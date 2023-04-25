afa02f15f2e0c80c67cbf50779693fae9757a0fd
Alex Nio <volpenio@gmail.com>
2023-04-25 16:15:03 +0500

    create rep

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup></script>
+
+<template>
+  <Page class="page">
+    <div class="page__container">
+      <h1>Hello world</h1>
+    </div>
+  </Page>
+</template>
+
+<style lang="scss" scoped>
+  h1 {
+    font-size: 4rem;
+    text-align: center;
+    line-height: 100%;
+    padding: 20vw;
+  }
+</style>
